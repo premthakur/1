5d1045ceb320d1a158c3f7cce71df882853ef9fc
Prem <ppthakur8483@gmail.com>
2023-09-26 18:14:21 +0530

    demo ka file add kiya hu

---
+++ demo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("kaise ho aap"),
+        ),
+        body: Text("kya karna hai"),
+      ),
+    );
+  }
+}
